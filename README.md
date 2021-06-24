f9c9072323c734d878c65369c68083b281573836
OLIET2357 <50865315+OLIET2357@users.noreply.github.com>
2021-06-24 18:41:09 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# DES
-Fast DES implementation by OLIET. Full compatibility with CUDA.
+# ABSTRACT
+
+Fast DES implementation by OLIET.
+
+Full compatibility with CUDA.
+
+Referred https://qiita.com/arsley/items/d2679f1a7e68d93c8dbe (Japanese)
+
+# USAGE
+Use via C++, compile "des.cpp", and include "des.hpp".
+
+Use via CUDA, DO NOT COMPILE "des.cpp" neither "des.hpp", just include "des.cpp".
+
+## Example Code
+### main.cpp
+    #include "des.hpp"
+    int main()
+    {
+        DES::main();
+        return 0;
+    }
+
+### Main.cu
+    #include "des.cpp"
+    int main()
+    {
+        DES::main();
+        return 0;
+    }
+
---
+++ des.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <cstdint>
+
+#define PREFIX
+
+using block_t = uint64_t;
+
+namespace DES
+{
+    void print_bit(uint64_t x, int bit);
+    PREFIX uint32_t popcnt(uint32_t x);
+    PREFIX int parity(unsigned int x);
+    PREFIX uint64_t translocate_PC1(const block_t k);
+    PREFIX block_t translocate_PC1I(const uint64_t kp);
+    PREFIX uint64_t translocate_PC2(const uint64_t cd);
+    PREFIX uint64_t translocate_IP(const block_t a);
+    PREFIX uint64_t translocate_E(const uint64_t r);
+    PREFIX uint32_t transform_SBOX(const uint64_t i);
+    PREFIX uint32_t translocate_P(const uint32_t j);
+    PREFIX uint32_t f(const uint64_t r, const uint64_t sk);
+    PREFIX block_t translocate_IPI(const uint64_t rl);
+    PREFIX void generate_subkeys(uint64_t sk[16], const uint64_t kp);
+    PREFIX block_t encrypt_block_with_subkeys_prime(const block_t pp, const uint64_t sk[16]);
+    PREFIX block_t decrypt_block_with_subkeys_prime(const block_t cp, const uint64_t sk[16]);
+    PREFIX block_t encrypt_block_with_subkeys(const block_t p, const uint64_t sk[16]);
+    PREFIX block_t decrypt_block_with_subkeys(const block_t c, const uint64_t sk[16]);
+    PREFIX block_t encrypt_block(const block_t p, const block_t k);
+    PREFIX block_t decrypt_block(const block_t c, const block_t k);
+    void main();
+}
+
+#undef PREFIX
